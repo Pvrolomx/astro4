da63350705e3c8472fbd5cd978369ad5a10711b8
Pvrolomx <pvrolo@gmail.com>
2026-01-09 16:24:44 -0600

    fix: footer selector correcto + texto visible - .footer a footer

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // ASTRO4 Service Worker
 // Cache-first strategy for offline support
 
-const CACHE_NAME = 'astro4-v1767996434';
+const CACHE_NAME = 'astro4-v1768006000';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
